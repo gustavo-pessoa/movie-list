2862160f2798662cecb16be46d9078e272bb569d
Gustavo Pessoa <gustavo.pessoa@gmail.com>
2021-03-15 22:39:09 +0000

    202103152239

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,34 +1,21 @@
-import { MovieCard } from "./MovieCard";
-import { useState } from 'react';
+
+import { GenreResponseProps, MovieProps } from '../App';
+
+import { MovieCard } from '../components/MovieCard';
 
 import '../styles/content.scss';
 
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
+interface ContentProps {
+  selectedGenre: GenreResponseProps,
+  movies: MovieProps[]
 }
 
-interface MovieProps {
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
-export function Content() {
-  const [selectedGenreId, setSelectedGenreId] = useState(1);
-
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
+export function Content({ 
+  selectedGenre,
+  movies
+}: ContentProps) {
   return (
-      <div className="container">
+    <div className="container">
         <header>
           <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
         </header>
@@ -40,6 +27,7 @@ export function Content() {
             ))}
           </div>
         </main>
-      </div>
+    </div>
+    
   )
 }
